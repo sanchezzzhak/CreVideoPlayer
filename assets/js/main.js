641f5e3d208bab61ae1304535a5d6c2a61473667
sanchezzzhak <sanchezzzhak@yandex.ru>
2017-05-13 11:11:04 +0300

    append jquery plugin

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,38 @@
-function CreVideoPlayer() {
-    this.init();
-};
+/***
+ * Cre Video Player (CreVPlayer)
+ */
+(function (root, factory) {
+    // CommonJS support
+    if (typeof exports === 'object') {
+        module.exports = factory();
+    }
+    // AMD
+    else if (typeof define === 'function' && define.amd) {
+        define(['jquery'], factory);
+    }
+    // Browser globals
+    else {
+        factory(root.jQuery);
+    }
+}(this, function ($) {
+    'use strict';
+    function CreVPlayer(element,options) {
+        this.init();
+    }
 
-CreVideoPlayer.prototype = {
-    constructor: CreVideoPlayer,
-    init: function(){}
-};
+    CreVPlayer.prototype = {
+        constructor: CreVPlayer,
+        init: function () {
+        }
+    };
+
+    // append jquery plugin
+    $.fn.CreVPlayer = function (option) {
+        var options = typeof option == 'object' && option;
+        new CreVideoPlayer(this, options);
+        return this;
+    };
+    $.fn.CreVPlayer.Constructor = CreVPlayer;
+
+
+}));
